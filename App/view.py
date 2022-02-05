b72c906db23d197ee5d6f58850ed1e6d5f00a2d8
Juan David Salguero Medina <j.salgueroniandes.edu.co>
2022-02-05 13:29:59 -0500

    Cambios del estudiante 1 hechos (Hechos en: Est-1)

--- App/view.py
+++ App/view.py
@@ -42,7 +42,8 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Modificación de Est-1 en el Lab 2
+    # TODO: Modificación de Est-1 en el Lab 2 (Hecho)
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -64,8 +65,9 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    # TODO: Modificación de Est-1 en el Lab 2
-    pass
+    # TODO: Modificación de Est-1 en el Lab 2 (Hecho)
+    
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 
 """
@@ -79,8 +81,8 @@ while True:
         books = loadBooks()
         print('Total de libros cargados: ' + str(lt.size(books)))
 
-        # TODO: Modificación de Est-1 en el Lab 2
-
+        # TODO: Modificación de Est-1 en el Lab 2 (Hecho)
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
         # TODO: Modificación de Est-2 en el Lab 2
 
